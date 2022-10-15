8b58cf9c733000fe5dbe9c04297a3325dafe3f5c
Kush Nee <kushnee5@gmail.com>
2022-10-15 12:45:03 +0800

    添加 custom.org 的配置文件，去除部分 package

--- init.el
+++ init.el
@@ -2,3 +2,9 @@
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (set 'ad-redefinition-action 'accept)
+
+;; https://www.reddit.com/r/emacs/comments/4q4ixw
+;; 一些更私人（计帐之类的）、实验性的插件安装到这里
+(org-babel-load-file (expand-file-name "custom.org" user-emacs-directory))
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
