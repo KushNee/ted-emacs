57b9ce6df755a1766a305cedc2997700d3754b41
Kush Nee <kushnee5@gmail.com>
2022-10-17 19:35:12 +0800

    custom.org/el 设置为可选项

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 
 ;; https://www.reddit.com/r/emacs/comments/4q4ixw
 ;; 一些更私人（计帐之类的）、实验性的插件安装到这里
-(org-babel-load-file (expand-file-name "custom.org" user-emacs-directory))
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file 'noerror)
+(when (file-exists-p (concat user-emacs-directory "custom.org"))
+  (org-babel-load-file (expand-file-name "custom.org" user-emacs-directory))
+)
+(when (file-exists-p (concat user-emacs-directory "custom.el"))
+  (setq custom-file (concat user-emacs-directory "custom.el"))
+  (load custom-file 'noerror)
+)
